6bd4ad5c021df0ba4362d8ad8e5bd982aecda150
Bri997 <brian.musial@gmail.com>
2021-07-08 21:35:19 -0500

    added VPC with an attached elastic ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,33 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloServer"
+    Name = "My Server"
+
+    Project = "my_devops_project"
+
+    Eviromnent = "test"
+
+    ManagedBy = "Terraform"
   }
+}
+resource "aws_eip" "my_app_eip" {
+
+
+  vpc = true
+
+  tags = {
+    Name = "My Server EIP"
+
+    Project = "my_devops_project"
+
+    Eviromnent = "test"
+
+    ManagedBy = "Terraform"
+  }
+
+}
+# allow the same IP address with shutdown of instance 
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.app_server.id
+  allocation_id = aws_eip.my_app_eip.id
 }
